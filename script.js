edd1cf65cb5b8d4e2c89857437f987b724868d06
Akram Shaik <akramroy813@gmail.com>
2025-06-21 16:36:39 +0530

    Update script.js
    rocket code removed

--- script.js
+++ script.js
@@ -262,20 +262,3 @@ window.addEventListener('touchmove', (e) => {
     moveRocket(touchX, touchY);
   }
 });
-
-// rocket code started
-function moveRocket(x, y) {
-  // Calculate angle based on center of rocket
-  const rocketCenterX = rocket.offsetLeft + rocket.offsetWidth / 2;
-  const rocketCenterY = rocket.offsetTop + rocket.offsetHeight / 2;
-
-  const dx = x - rocketCenterX;
-  const dy = y - rocketCenterY;
-  const angle = Math.atan2(dy, dx) * 180 / Math.PI + 90;
-
-  // Move and rotate the rocket
-  rocket.style.left = (x - 25) + 'px';  // Adjust for half width
-  rocket.style.top = (y - 25) + 'px';   // Adjust for half height
-  rocket.style.transform = `rotate(${angle}deg)`;
-}
-// rocket code ended
